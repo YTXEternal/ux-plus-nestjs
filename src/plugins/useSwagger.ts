e973651f74a5a3a906690432feb0fb74b17ba622
YTXEternal <3781216085@qq.com>
2026-02-02 06:21:20 +0800

    docs: 导出swagger.json

--- src/plugins/useSwagger.ts
+++ src/plugins/useSwagger.ts
@@ -1,5 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export const useSwagger = (app: INestApplication<any>) => {
   if (process.env.NODE_ENV === 'production') return;
@@ -9,5 +11,12 @@ export const useSwagger = (app: INestApplication<any>) => {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
+
+  // 导出 Swagger JSON 文件到根目录
+  fs.writeFileSync(
+    path.resolve(process.cwd(), 'swagger.json'),
+    JSON.stringify(document, null, 2),
+  );
+
   SwaggerModule.setup('swagger', app, document);
 };
